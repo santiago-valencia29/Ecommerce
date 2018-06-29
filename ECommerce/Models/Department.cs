3e0b233ced0d5804649753f6e49abc38ab21f6ff
santi <santi@DESKTOP-14RNQGS>
2018-06-29 18:45:08 -0500

    Departments and Cities basic CRUD

---
+++ ECommerce/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="The field {0} is required")]
+        [MaxLength(50,ErrorMessage = "The filed {0} must be maximun {1} characters length")]
+        [Display(Name="Department")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City>  Cities { get; set; }
+    }
+}
---
+++ ECommerce/Models/ECommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class ECommerceContext: DbContext
+    {
+        public ECommerceContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ECommerce.Models.Department> Departments { get; set; }
+
+        public System.Data.Entity.DbSet<ECommerce.Models.City> Cities { get; set; }
+    }
+}
